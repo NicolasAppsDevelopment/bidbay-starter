707e5f2ef53ad8ad7541d617063cbc2cef2aaa3d
TOM <you@example.com>
2024-03-29 12:38:15 +0100

    get product v1 (need to be tested)

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -2,11 +2,26 @@ import express from 'express'
 import { Product, Bid, User } from '../orm/index.js'
 import authMiddleware from '../middlewares/auth.js'
 import { getDetails } from '../validators/index.js'
+import { Sequelize } from 'sequelize'
 
 const router = express.Router()
 
 router.get('/api/products', async (req, res, next) => {
-  res.status(600).send()
+  const products = await Product.findAll(
+    {attributes: [
+      'id',
+      'name',
+      'description',
+      'category',
+      'originalPrice',
+      [Sequelize.literal(`(SELECT price FROM Bid WHERE productId = Product.id AND date=max(date))`), 'lastPrice'],
+      'pictureUrl',
+      'endDate',
+      [Sequelize.literal(`(SELECT id,username FROM User WHERE id = Product.sellerId`), 'seller'],
+      [Sequelize.literal(`(SELECT id,price,date FROM Bid WHERE productId = Product.id `), 'bids']]
+    }
+  );
+  res.status(200).send(products)
 })
 
 router.get('/api/products/:productId', async (req, res) => {
