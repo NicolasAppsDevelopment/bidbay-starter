2d82dfccf2202e5992638dba87fd024ac04a5036
TOM <you@example.com>
2024-04-08 11:02:26 +0200

    product + reset

--- backend/routes/dev.ts
+++ backend/routes/dev.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import { regenerateFixtures } from '../orm/fixtures/index.js'
+import { Bid, Product, User } from '../orm/index.js'
 
 const router = express.Router()
 
 router.get('/api/dev/reset', async (req, res) => {
-  await regenerateFixtures()
-  res.status(200).send('OK')
+  await regenerateFixtures();
+  await Bid.truncate();
+  await Product.truncate();
+  await User.truncate();
+  res.status(200).send('OK');
 })
 
 export default router
--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -42,7 +42,7 @@ router.get('/api/products', async (req, res, next) => {
   });
 
   
-  res.status(200).send(products)
+  res.status(200).send(products);
 })
 
 router.get('/api/products/:productId', async (req, res) => {
@@ -68,33 +68,54 @@ router.get('/api/products/:productId', async (req, res) => {
       {
         model:Bid,
         as: 'bids',
-        attributes:['id','price','date',include: [
+        attributes:['id','price','date'],
+        include: [
           {
             model:User,
             as: 'bidder',
             attributes:['id','username'] 
           }
-        ] ] 
+        ]
       }
-
     ]
-    
-});
-  res.status(200).send(products)
+  });
+
+  if (products==null){
+    res.status(404).json({ error: 'not found'});
+  } 
+
+  res.status(200).send(products);
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
 router.post('/api/products', (req, res) => {
-  res.status(600).send()
+  
+  //if not register as an user
+  //req.user.id
+  //res.status(401).send("not authorized");
+
+  //if (req.params.name==null || ... )
+  
+  const products = Product.create({
+      name:req.body.name,
+      description:req.body.description,
+      category:req.body.category,
+      originalPrice:req.body.originalPrice,
+      pictureUrl:req.body.pictureUrl,
+      endDate:req.body.endDate,
+  })
+
+
+  res.status(201).send("created");
 })
 
 router.put('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  res.status(600).send();
 })
 
 router.delete('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  res.status(600).send();
 })
 
 export default router
